e29b9a4a22f4a21df29dd0028c9f1bb985dd9164
Bassam QORMOSH <b.qormosh@unrwa.org>
2022-01-01 20:47:06 +0200

    initial logic for registeration

--- PodcastApp.WebUI/Controllers/AccountController.cs
+++ PodcastApp.WebUI/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PodcastApp.Domain.Interfaces;
+using PodcastApp.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +14,14 @@ namespace PodcastApp.WebUI.Controllers
     {
         private readonly ILogger<AccountController> _logger;
         private readonly IUnitOfWork _unitOfWork;
-        public AccountController(ILogger<AccountController> logger, IUnitOfWork unitOfWork)
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        public AccountController(ILogger<AccountController> logger, IUnitOfWork unitOfWork, UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager)
         {
             _logger = logger;
             _unitOfWork = unitOfWork;
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -30,6 +36,30 @@ namespace PodcastApp.WebUI.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = registerViewModel.Email, Email = registerViewModel.Email };
+                var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index","Home");
+                }
+                // if not redirected then there are errors
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(registerViewModel);
+
         }
     }
 }
